ccf58c95276e5ec7fb5667dfe60992e6c25df89a
Octanne <contact@octanne.eu>
2022-09-07 23:12:10 +0200

    Fix maitre in CMD block

--- src/main/java/eu/octanne/mcboyard/modules/Maitre.java
+++ src/main/java/eu/octanne/mcboyard/modules/Maitre.java
@@ -1,6 +1,8 @@
 package eu.octanne.mcboyard.modules;
 
 import org.bukkit.Bukkit;
+import org.bukkit.block.CommandBlock;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -25,7 +27,7 @@ public class Maitre extends PlugModule {
 		}
 		scoreboard.registerNewTeam("Maitre");
 		scoreboard.getTeam("Maitre").setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.NEVER);
-		
+
 		pl.getCommand("maitre").setExecutor(new MaitreCommand());
 	}
 	public void onDisable() {
@@ -36,6 +38,27 @@ public class Maitre extends PlugModule {
 
 		@Override
 		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+			if (sender instanceof BlockCommandSender) {
+				double lastDistance = Double.MAX_VALUE;
+				Player result = null;
+				for(Player p : ((BlockCommandSender) sender).getBlock().getWorld().getPlayers()) {
+					double distance = ((BlockCommandSender) sender).getBlock().getLocation().distance(p.getLocation());
+					if(distance < lastDistance) {
+						lastDistance = distance;
+						result = p;
+					}
+				}
+
+				if(scoreboard.getTeam("Maitre").hasEntry(result.getName())) {
+					scoreboard.getTeam("Maitre").removeEntry(result.getName());
+					result.sendMessage("§8Vous venez de quitter les maîtres du temps.");
+					return true;
+				}else {
+					scoreboard.getTeam("Maitre").addEntry(result.getName());
+					result.sendMessage("§8Vous êtes desormais maître du temps.");
+					return true;
+				}
+			}
 			if(args.length < 1 && !(sender instanceof Player)) {
 				sender.sendMessage("Veulliez préciser un joueur : /Maitre <joueur>");
 				return false;
